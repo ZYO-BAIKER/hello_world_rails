3261b4f9a0a076e0aa3606de224c88c23c928f3e
ZYO-BAIKER <keisukeminakawa@gmail.com>
2021-11-02 10:44:39 +0900

    create, update, destroy の API が実際に動作するように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,39 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:update,:destroy]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    ## インスタンスを DB に保存する
+    @user.save!
+    ## json として値を返す
+    render :show
+  end
+
+  def update
+    # 探してきたレコードに対して変更を行う
+    @user.update!(user_params)
+    # json として値を返す
+    render :show
+  end
+
+  def destroy
+    @user.destroy!
+    render :show # json として値を返す
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id]) # 対象のレコードを探す
+  end
+
+  def user_params
+    params.require(:user).permit(:name,:account,:email)
   end
 end
